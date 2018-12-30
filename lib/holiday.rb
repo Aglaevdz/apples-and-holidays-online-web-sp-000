73b8658d41593615fe483fed991fedbf53602f60
Aglae Verduzco <aglae.cv@gmail.com>
2018-12-30 02:02:39 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -51,7 +51,12 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |season, holiday|
+    puts "#{season.capitalize}:"
+  holiday.each do |holiday_name, supplies|
+    puts "  #{holiday_name}"
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
